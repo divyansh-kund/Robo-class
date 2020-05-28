1c5a490395a8e19798bd6133a858d0f53fd79bbb
divyansh-kund <divyanshkundu8@gmail.com>
2020-05-28 09:31:45 +0530

    webpage still not working

---
+++ precache-manifest.766268b47bfc555d3b66c599597c006d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b66dc84b866066768e93fc99d8791ca",
+    "url": "/Robo-Class/index.html"
+  },
+  {
+    "revision": "98f849ad1162e1b4a42b",
+    "url": "/Robo-Class/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "80b37f842051b503e880",
+    "url": "/Robo-Class/static/css/main.8bf153c1.chunk.css"
+  },
+  {
+    "revision": "98f849ad1162e1b4a42b",
+    "url": "/Robo-Class/static/js/2.31eed32d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Robo-Class/static/js/2.31eed32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b37f842051b503e880",
+    "url": "/Robo-Class/static/js/main.f1b1f1ef.chunk.js"
+  },
+  {
+    "revision": "caec41564d35bc08ef78",
+    "url": "/Robo-Class/static/js/runtime-main.45b4019e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Class/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60fd1634fcb955a6fad81fa29925720a.js"
+  "/Robo-Class/precache-manifest.766268b47bfc555d3b66c599597c006d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Robo-Class/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
