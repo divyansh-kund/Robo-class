db4e33291ac00e0a14a237f27e4fa8ba7fab75bb
divyansh-kund <divyanshkundu8@gmail.com>
2020-05-28 08:34:00 +0530

    Adding starting project

---
+++ precache-manifest.60fd1634fcb955a6fad81fa29925720a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a8240a48e11e0fd28594b6b643b016",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98f849ad1162e1b4a42b",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "46c583b09b2969eb6f37",
+    "url": "/static/css/main.b8d33388.chunk.css"
+  },
+  {
+    "revision": "98f849ad1162e1b4a42b",
+    "url": "/static/js/2.31eed32d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31eed32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c583b09b2969eb6f37",
+    "url": "/static/js/main.877f4f1c.chunk.js"
+  },
+  {
+    "revision": "aca0f5dabab3afe36e0f",
+    "url": "/static/js/runtime-main.e96107c2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
